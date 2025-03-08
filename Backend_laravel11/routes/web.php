2151d30cfe54b8a86ece3fc37226fa55d46cbb7c
lupinlic <huatunglam1205@gmail.com>
2025-03-08 15:36:10 +0700

    update fe

--- Backend_laravel11/routes/web.php
+++ Backend_laravel11/routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-// use App\Http\Controllers\ProfileController;
-// use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
--- frontend/src/components/shared/ProductFrame/index.js
+++ frontend/src/components/shared/ProductFrame/index.js
@@ -8,13 +8,13 @@ import { apiUrl } from '../../../config';
 const ProductFrame = ({ products }) => {
 
     const getImagePath = (productImg) => {
-                try {
-                  return `${apiUrl}/uploads/Categories/${productImg}`;
-                } catch (error) {
-                  console.error('Error loading image:', error);
-                  return null; // Hoặc có thể trả về một hình ảnh mặc định
-                }
-              };
+        try {
+            return `${apiUrl}/uploads/Categories/${productImg}`;
+        } catch (error) {
+            console.error('Error loading image:', error);
+            return null; // Hoặc có thể trả về một hình ảnh mặc định
+        }
+    };
 
     return (
         <>
@@ -23,13 +23,14 @@ const ProductFrame = ({ products }) => {
                     <div className="col-md-2 text-center pro-item p-1 position-relative" key={product.id} style={{ background: '#908f8f', marginBottom: '24px' }}>
                         <div className='pro-item_child' style={{ width: '100%', height: '100%', position: 'absolute', background: '#fff', top: '1%', padding: '8px' }}>
                             <img
+                                style={{ height: '195.66px', width: '100%' }}
                                 src={getImagePath(product.ImageURL)}
                                 alt={product.name}
-                                className="w-100 mb-2"
+                                className=" mb-2"
                             />
-                            <h5>{product.Name}</h5>
+                            <h5 style={{ height: '48px' }}>{product.Name}</h5>
                             <p className="text-danger">{product.Price}</p>
-    
+
                         </div>
                         <Link to='/ProductDetails'><button style={{ position: 'absolute', bottom: '-5%', left: '30%', border: 'none', borderRadius: '5px', background: '#bd8133', color: '#fff', padding: '5px' }}>Xem chi tiết</button></Link>
                     </div>
